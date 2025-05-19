87a5a2326d7b4b69a52960061dac80963347cc22
DeaRadita <draditaindayani@gmail.com>
2025-05-19 11:29:37 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,23 @@
+from operasi import tambah, kurang, kali, bagi
+
+print("APLIKASI KALKULATOR SEDERHANA")
+print("1. Tambah")
+print("2. Kurang")
+print("3. Kali")
+print("4. Bagi")
+
+pilih = input("Pilih operasi (1/2/3/4): ")
+a = float(input("Masukkan angka pertama: "))
+b = float(input("Masukkan angka kedua: "))
+
+if pilih == '1':
+    print("Hasil:", tambah(a, b))
+elif pilih == '2':
+    print("Hasil:", kurang(a, b))
+elif pilih == '3':
+    print("Hasil:", kali(a, b))
+elif pilih == '4':
+    print("Hasil:", bagi(a, b))
+else:
+    print("Pilihan tidak valid")
 
